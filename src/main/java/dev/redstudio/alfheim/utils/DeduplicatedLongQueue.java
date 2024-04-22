cb2c65a26307af96bd980f802974f78913b0139f
thatdude <102991577+thatdudew@users.noreply.github.com>
2024-04-22 13:08:36 +0100

    Update DeduplicatedLongQueue.java

--- src/main/java/dev/redstudio/alfheim/utils/DeduplicatedLongQueue.java
+++ src/main/java/dev/redstudio/alfheim/utils/DeduplicatedLongQueue.java
@@ -57,7 +57,7 @@ public final class DeduplicatedLongQueue {
     /**
      * Clears the deduplication set.
      */
-    public void clearSet() {
-        set.clear();
-    }
+   public void clear() {
+    set.clear();
+    queue.clear();
 }
